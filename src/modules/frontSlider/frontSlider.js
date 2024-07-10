b4464f8fd06314d34f2ef1060f4c6e175a9f9db9
kirillzukov <kukkoman@gmail.com>
2024-07-10 20:29:12 +0300

    progress

--- src/modules/frontSlider/frontSlider.js
+++ src/modules/frontSlider/frontSlider.js
@@ -1,29 +1,29 @@
-import initSlider from "../../js/initSlider";
-import { BREAKPOINT_LG } from "src/js/breakpoints";
-
-initSlider('.frontSlider__slider', ( $el ) => ({
-  wrapperClass: 'frontSlider__slides',
-  slideClass:   'frontSlider__slide',
-  // direction:    'vertical',
-  navigation: {
-    //   prevEl: $el.parents('.hints').find('.hints__nav--prev')[0],
-    nextEl: $el.parents('.frontSlider').find('.frontSlider__button')[0],
-  },
-  // pagination:      {
-  //   el:        $el.parents('.frontSlider').find('.frontSlider__pagination')[0],
-  //   clickable: true,
-  // },
-  // touchEventsTarget: 'wrapper',
-  slidesPerView: 1,
-  spaceBetween:  0,
-
-  direction:   'vertical',
-  allowTouchMove: false,
-  breakpoints: {
-    [BREAKPOINT_LG]: {
-      direction: 'horizontal',
-  allowTouchMove: true
-    }
-  }
-  // autoHeight:    true,
-}))
+// import initSlider from "../../js/initSlider";
+// import { BREAKPOINT_LG } from "src/js/breakpoints";
+//
+// initSlider('.frontSlider__slider', ( $el ) => ({
+//   wrapperClass: 'frontSlider__slides',
+//   slideClass:   'frontSlider__slide',
+//   // direction:    'vertical',
+//   navigation: {
+//     //   prevEl: $el.parents('.hints').find('.hints__nav--prev')[0],
+//     nextEl: $el.parents('.frontSlider').find('.frontSlider__button')[0],
+//   },
+//   // pagination:      {
+//   //   el:        $el.parents('.frontSlider').find('.frontSlider__pagination')[0],
+//   //   clickable: true,
+//   // },
+//   // touchEventsTarget: 'wrapper',
+//   slidesPerView: 1,
+//   spaceBetween:  0,
+//
+//   direction:   'vertical',
+//   allowTouchMove: false,
+//   breakpoints: {
+//     [BREAKPOINT_LG]: {
+//       direction: 'horizontal',
+//   allowTouchMove: true
+//     }
+//   }
+//   // autoHeight:    true,
+// }))
